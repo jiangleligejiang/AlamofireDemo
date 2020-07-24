f111cfbf89a172f4e5877017fc4a3e881594f1f1
jams <jams@163.com>
2020-07-24 14:26:09 +0800

    # 引入Alamofire库

---
+++ AlamofireDemo/AlamofireDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireDemo
+//
+//  Created by jams on 2020/7/24.
+//  Copyright © 2020 jams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    
+}
+
